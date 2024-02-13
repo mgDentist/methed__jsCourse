43396b665341001421b9d5ff4521119c375981e8
mgDentist <gmi1987@gmail.com>
2024-02-13 11:26:33 +0400

    module1 theme1

---
+++ main.js
@@ -0,0 +1,15 @@
+// Наименование товара
+const productName = 'Березовый сок';
+
+// Количество товара
+const productAmount = 5;
+
+// Категория товара
+const productCategory = 'liquids';
+
+// Цена товара
+const productPrice = 1000;
+
+console.log('Товар:', productName);
+
+console.log('Общая сумма товара:', productPrice * productAmount, 'рублей');

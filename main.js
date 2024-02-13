72602e2f0fb673c7b47af4448287cdf1e111c47f
mgDentist <gmi1987@gmail.com>
2024-02-13 17:02:14 +0400

    module1 theme2

--- main.js
+++ main.js
@@ -1,15 +1,36 @@
-// Наименование товара
-const productName = 'Березовый сок';
+'use strict';
+{
+    // Наименование товара
+    const productName = 'Березовый сок';
 
-// Количество товара
-const productAmount = 5;
+    // Количество товара
+    const productAmount = 5;
 
-// Категория товара
-const productCategory = 'liquids';
+    // Категория товара
+    const productCategory = 'liquids';
 
-// Цена товара
-const productPrice = 1000;
+    // Цена товара
+    const productPrice = 1000;
 
-console.log('Товар:', productName);
+    console.log('Товар:', productName);
 
-console.log('Общая сумма товара:', productPrice * productAmount, 'рублей');
+    console.log(`Общая сумма товара при заказе ${productAmount} литров: ${productPrice * productAmount} руб`);
+}
+
+{
+    // Наименование товара
+    const productName = 'Томатный сок';
+
+    // Количество товара
+    const productAmount = 13;
+
+    // Категория товара
+    const productCategory = 'liquids';
+
+    // Цена товара
+    const productPrice = 128;
+
+    console.log('Товар:', productName);
+
+    console.log(`Общая сумма товара при заказе ${productAmount} литров: ${productPrice * productAmount} руб`);
+}
